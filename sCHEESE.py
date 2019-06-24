9d37d405c205232d5db1a7d2a8d5bc0823a21792
Andrew Hua <andrewhua90@gmail.com>
2019-06-24 17:56:23 -0400

    changes

--- sCHEESE.py
+++ sCHEESE.py
@@ -17,61 +17,67 @@ def delay(milliseconds):
 # BASIC INPUT/OUTPUT (IO)
 #########
 
-class inputPin: # inputPin will allow user to assign a pin for input purposes(reads in something from the outside)
+import RPi.GPIO as GPIO
+
+# disables warning, not sure if this is bad or not
+GPIO.setwarnings(False)
+
+# it's possible the entire output / input pin can be integrated into one class, will depend on the HAT's capabilities
+ 
+# inputPin will allow user to assign a pin for input purposes(reads in something from the outside)
+class inputPin:
     
     valid = True
     
+    # constructor
     def __init__(self, pinNum):  
-        #import RPi.GPIO as GPIO  consider moving import RPi.GPIO out of the init for the classes 
         self.valid = True
-        import RPi.GPIO as GPIO
         GPIO.setmode(GPIO.BOARD)
-        self.GPIO = GPIO
         
         # maps the pins for the HAT's connections to the J8 RPi numbering system
         mapping = { 0 : 29,
-                                1 :  31,
-                                2 :  33,
-                                3 : 35,
-                                4 : 37,
-                                5 : 32,
-                                6 : 36,
-                                7 : 38}
+                    1 : 31,
+                    2 : 33,
+                    3 : 35,
+                    4 : 37,
+                    5 : 32,
+                    6 : 36,
+                    7 : 38}
         if pinNum >= 0 and pinNum <= 7:
             self.pinNum = mapping[pinNum]
             GPIO.setup(self.pinNum, GPIO.OUT);
         else:
             self.valid = False
     
+    # reads in current electrical signal value from the assigned pin
     def pinRead(self):
         if self.valid:
             print('you reading')
-            return self.GPIO.input(self.pinNum); # not sure if  I should use self. prefix or not
+            return GPIO.input(self.pinNum); # not sure if  I should use self. prefix or not
         else:
             print('not the right word')    
     
-    def clean(self):
-        self.GPIO.cleanup()
+    # destructor
+    def __del__(self):
+        GPIO.cleanup()
 
-# it's possible the entire output / input pin can be integrated into one class, will depend on the HAT's capabilities
+# outputPin will allow user to assign a pin for outputpurposes(reads in something from the outside)
 class outputPin:
 
     valid = True
 
     def __init__(self, pinNum):
         self.valid = True
-        import RPi.GPIO as GPIO
         GPIO.setmode(GPIO.BOARD)
-        self.GPIO = GPIO
         
         mapping = { 0 : 29,
-                                1 :  31,
-                                2 :  33,
-                                3 : 35,
-                                4 : 37,
-                                5 : 32,
-                                6 : 36,
-                                7 : 38}
+                    1 : 31,
+                    2 : 33,
+                    3 : 35,
+                    4 : 37,
+                    5 : 32,
+                    6 : 36,
+                    7 : 38}
         
         if pinNum >= 0 and pinNum <= 7:
             self.pinNum = mapping[pinNum]
@@ -82,32 +88,34 @@ class outputPin:
     def pinWrite(self, state):
         if self.valid:
             if state.lower() == 'high':
-                self.GPIO.output(self.pinNum, self.GPIO.HIGH)
+                GPIO.output(self.pinNum, GPIO.HIGH)
                 print('this is high')
             elif state.lower() == 'low':
-                self.GPIO.output(self.pinNum, self.GPIO.LOW)
+                GPIO.output(self.pinNum, GPIO.LOW)
                 print('this is low')
             else:
                 print('not the right word')
         
-    def clean(self):
-        self.GPIO.cleanup()            
+    def __del__(self):
+        GPIO.cleanup()
 
 #########
 # PWM WITH I2C
 #########
 
 
-from board import SCL, SDA
+### test when we have the board ready
+'''
+import board
 import busio
 
-from adafruit_pca9685 import PCA9685
+import adafruit_pca9685
 
 # generates the I2C bus interface
-i2c_bus = busio.I2C(SCL,SDA)
+i2c_bus = busio.I2C(board.SCL,board.SDA)
 
 # pca object for the circuit
-pca = PCA9685(i2c_bus)
+pca = adafruit_pca9685.PCA9685(i2c_bus)
 
 pca.frequency = 60
 
@@ -115,6 +123,81 @@ pca.frequency = 60
 # SERVO
 #########
 
-from adafruit_servokit import ServoKit
+import adafruit_servokit
 
 kit = ServoKit(channels=16)
+'''
+
+#########
+# MOTOR
+#########
+
+# Credit to nick-hunter on github for example code for a Python library for the TB6612FNG Motor
+
+class motor:
+    
+    # input channels for the motors
+    in1 = ''
+    in2 = ''
+    
+    # pulse width modulation for the 'analog' value
+    pwm = ''
+    standbyPin = ''
+    
+    # frequency used for the PWM
+    hertz = 1000
+    
+    # constructor for the motor object
+    def __init__ (self, in1, in2, pwm, standbyPin):
+        GPIO.setmode(board)
+        self.in1 = in1
+        self.in2 = in2
+        self.pwm = pwm
+        self.standbyPin = standbyPin
+        self.reverse = reverse
+        
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        GPIO.setup(self.pwm, GPIO.OUT)
+        GPIO.setup(self.standbyPin, GPIO.OUT)
+        self.p = GPIO.PWM(pwm, self.hertz)
+        self.p.start(0)
+        
+    # function responsible for the movement of the motor (with speed between -100 and 100)
+    def move(self, speed):
+        dutyCycle = speed
+        
+        # initiates reverse movement
+        if (speed < 0):
+            dutyCycle = dutyCycle * -1
+            
+        # outputs movement of the motor
+        if (speed > 0):
+            GPIO.output(self.in1, GPIO.HIGH)
+            GPIO.output(self.in2, GPIO.LOW)
+        else:
+            GPIO.output(self.in1, GPIO.LOW)
+            GPIO.output(self.in2, GPIO.HIGH)
+            
+        self.p.ChangeDutyCycle(dutyCycle)
+        
+    # stops the motor from moving
+    def stop(self):
+        self.p.ChangeDutyCycle(0)
+        GPIO.output(self.in1,GPIO.HIGH)
+        GPIO.output(self.in2,GPIO.HIGH)
+        
+    # standby ?? not exactly sure what the purpose of this is, will remain to be seen if we will keep
+    def standby(self):
+        self.p.ChangeDutyCycle(0)
+        GPIO.output(self.standbyPin, value)
+        
+    # destructor
+    def __del__(self):
+        GPIO.cleanup()
+        
+#########
+# ADC (Analog-to-Digital-Converter)
+#########
+
+
