fe1b335163f178e4d5d772dc6df0ccade92d2a66
Andrew Hua <andrewhua90@gmail.com>
2019-07-04 15:25:17 -0400

    fixed some stuff

--- sCHEESE.py
+++ sCHEESE.py
@@ -12,14 +12,14 @@ import time
 
 def delay(milliseconds):
     time.sleep(milliseconds / 1000)
-    print('time delay of  {0} millisecond(s)'.format(milliseconds/1000))
+    #print('time delay of  {0} millisecond(s)'.format(milliseconds/1000))
 
 # The delayMicro function will mimic the same function in Arduino, with the purpose of being even shorter
 # amount of time, intended for unique circumstances and hardware
 
 def delayMicro(microseconds):
     time.sleep(microseconds/1000000)
-    print('time delay of {0} microsecond(s)'.format(microseconds/1000000))
+    #print('time delay of {0} microsecond(s)'.format(microseconds/1000000))
 
 #######
 # INPUT (MCP3008)
@@ -38,6 +38,7 @@ SPI_PORT = 0
 SPI_DEVICE = 0
 mcp = MCP.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
+# Considering having individual input classes for different types of modules
 class inputPort:
 
     # Validity flag to ensure that correct port is used
@@ -58,13 +59,13 @@ class inputPort:
 
     # Function to print the active value that is being read by the output IN ANALOG terms
     def printValue(self):
-        if valid:
+        if self.valid:
             print('Currently reading in a value of: ', mcp.read_adc(self.portNum))
 
     # Function responsible for detecting whether a button input has been received
     # Can be used as a conditional statement to gate whether a button input has been received in the code
     def buttonPress(self):
-        if valid:
+        if self.valid:
             # Unsure about the correct analog value threshold to use as typically input goes above 1000
             if (mcp.read_adc(self.portNum) > 800):
                 print('Button has been pressed')
@@ -75,7 +76,7 @@ class inputPort:
 
     # Function responsible for simply returning the current value of the input (useful for conditionals)
     def currValue(self):
-        if valid:
+        if self.valid:
             return mcp.read_adc(self.portNum)
 
 #######
@@ -135,7 +136,7 @@ class outputPort:
 
     # Testing function to see if the LED attached can turn on and off
     def onAndOff(self):
-        if valid:
+        if self.valid:
             for i in range(10000):
                 self.channel.duty_cycle = i
 
@@ -144,7 +145,7 @@ class outputPort:
 
     # Function associated with turning on or off the LED
     def ledStatus(self, instruction):
-        if valid:
+        if self.valid:
             if instruction.lower() == 'on':
                 self.channel.duty_cycle = 0x7fff
             elif instruction.lower() == 'off':
@@ -156,11 +157,22 @@ class outputPort:
     # gauge the amount of voltage that would be received through the PWM
     # Accepted values only from 0 to 255, which is then mapped to 0 to ~1023
     def ledLevel(self, level):
-        if valid:
-            if (level >= 0 && level < 256)
+        if self.valid:
+            if (level >= 0 and level < 256):
                 self.channel.duty_cycle = level * 256
             else:
                 print('invalid level')
+
+    def setPWM(self, pwm):
+        if self.valid:
+            if pwm >= 0:
+                self.channel.duty_cycle = pwm
+            else:
+                print ('invalid pwm')
+
+    def clear(self):
+        if self.valid:
+            self.channel.duty_cycle = 0
 
 #######
 # MOTOR OUTPUT (TB6612FNG)
@@ -210,7 +222,7 @@ class motor:
     # If it is negative, it will rotate the motor counter-clockwise
     # Accepted values only range from -256 to 256, exclusive
     def motorSpeed(self, speed):
-        if valid:
+        if self.valid:
             if (speed >= 0 and speed < 256):
                 self.channel1.duty_cycle = 0
                 self.channel2.duty_cycle = 0xffff
@@ -224,222 +236,65 @@ class motor:
 
     # Function for completely stopping the motor drivers, simply by setting all the channels to 0
     def motorStop(self):
-        if valid:
+        if self.valid:
+            self.pwmChannel = 0
+
+    def clear(self):
+        if self.valid:
             self.channel1.duty_cycle = 0
             self.channel2.duty_cycle = 0
             self.pwmChannel = 0
 
-'''
-# Will incorporate GPIO back into the library if needed
+import math
 
-#########
-# BASIC INPUT/OUTPUT (IO)
-#########
+class ultrasonicSensor:
 
-# I/O section using the RPi.GPIO library
-import RPi.GPIO as GPIO
-
-# disables warning, not sure if this is bad or not
-GPIO.setwarnings(False)
-
-# it's possible the entire output / input pin can be integrated into one class, will depend on the HAT's capabilities
- 
-# inputPin will allow user to assign a pin for input purposes(reads in something from the outside)
-class inputPin:
-    
     valid = True
-    
-    # constructor
-    def __init__(self, pinNum):  
-        self.valid = True
-        GPIO.setmode(GPIO.BOARD)
-        
-        # maps the pins for the HAT's connections to the J8 RPi numbering system
-        mapping = { 0 : 29,
-                    1 : 31,
-                    2 : 33,
-                    3 : 35,
-                    4 : 37,
-                    5 : 32,
-                    6 : 36,
-                    7 : 38}
-        if pinNum >= 0 and pinNum <= 7:
-            self.pinNum = mapping[pinNum]
-            GPIO.setup(self.pinNum, GPIO.OUT);
+
+    trigPort = -1
+    echoPort = -1
+    channel = -1
+
+    def __init__(self, trigPort, echoPort):
+
+        if (trigPort >= 0 and trigPort <= 7 and echoPort >= 0 and echoPort <= 7):
+            self.valid = True
+
+            self.echoPort = echoPort
+
+            outputMapping = {0: 6,
+                            1: 7,
+                            2: 8,
+                            3: 9,
+                            4: 10,
+                            5: 11,
+                            6: 12,
+                            7: 13}
+
+            self.trigPort = outputMapping[trigPort]
+            self.channel = pca.channels[self.trigPort]
         else:
             self.valid = False
-    
-    # reads in current electrical signal value from the assigned pin
-    def pinRead(self):
-        if self.valid:
-            print('you reading')
-            return GPIO.input(self.pinNum); # not sure if  I should use self. prefix or not
-        else:
-            print('not the right word')    
-    
-    # destructor
-    def __del__(self):
-        GPIO.cleanup()
 
-# outputPin will allow user to assign a pin for output purposes(reads in something from the outside)
-class outputPin:
+    def detect(self):
+        self.channel.duty_cycle = 0
+        delayMicro(2)
+        self.channel.duty_cycle = 0xffff
+        delayMicro(10)
+        self.channel.duty_cycle = 0
 
-    valid = True
+        startTime = 0
+        endTime = 0
 
-    def __init__(self, pinNum):
-        self.valid = True
-        GPIO.setmode(GPIO.BOARD)
-        
-        mapping = { 0 : 29,
-                    1 : 31,
-                    2 : 33,
-                    3 : 35,
-                    4 : 37,
-                    5 : 32,
-                    6 : 36,
-                    7 : 38}
-        
-        if pinNum >= 0 and pinNum <= 7:
-            self.pinNum = mapping[pinNum]
-            GPIO.setup(self.pinNum, GPIO.OUT);
-        else:
-            self.valid = False
-    
-    def pinWrite(self, state):
-        if self.valid:
-            if state.lower() == 'high':
-                GPIO.output(self.pinNum, GPIO.HIGH)
-                print('this is high')
-            elif state.lower() == 'low':
-                GPIO.output(self.pinNum, GPIO.LOW)
-                print('this is low')
-            else:
-                print('not the right word')
-        
-    def __del__(self):
-        GPIO.cleanup()
+        while mcp.read_adc(self.echoPort) == 0xffff:
+            startTime = time.time()
 
-#########
-# BASIC INPUT/OUTPUT (IO) 2.0
-#########
+        while not mcp.read_adc(self.echoPort) == 0xffff:
+            endTime = time.time()
 
-# commanding I/O using the gpiozero library
-# at the moment, not sure which library is better / compatible with the HAT
-import gpiozero as gp0
+        duration = endtime - startTime
 
-class pinInit:
+        distance = duration
 
-    valid = True
+        return distance
 
-    def __init__(self, pinNum):
-        self.pinNum = pinNum
-
-    def led(self, status):
-        diode = gp0.LED(self.pinNum)
-        if state.lower() == 'high':
-            diode.on()
-        elif state.lower() == 'low':
-            diode.off()
-        elif state.lower() == 'blink':
-            diode.blink()
-
-    def button(self, pinNum):
-        btn = gp0.BUTTON(self.pinNum)
-        if btn.is_pressed:
-            return True
-        else:
-            return False
-
-    def waitForButton(self, pinNum):
-        btn = gp0.BUTTON(self.pinNum)
-        while btn.wait_for_press():
-            time.sleep(1)
-
-#########
-# PWM WITH I2C
-#########
-
-
-### test when we have the board ready
-
-import board
-import busio
-
-import adafruit_pca9685
-
-# generates the I2C bus interface
-i2c_bus = busio.I2C(board.SCL,board.SDA)
-
-# pca object for the circuit
-pca = adafruit_pca9685.PCA9685(i2c_bus)
-
-pca.frequency = 60
-
-#########
-# MOTOR
-#########
-
-# Credit to nick-hunter on github for example code for a Python library for the TB6612FNG Motor
-
-class motor:
-    
-    # input channels for the motors
-    in1 = ''
-    in2 = ''
-    
-    # pulse width modulation for the 'analog' value
-    pwm = ''
-    standbyPin = ''
-    
-    # frequency used for the PWM
-    hertz = 1000
-    
-    # constructor for the motor object
-    def __init__ (self, in1, in2, pwm, standbyPin):
-        GPIO.setmode(board)
-        self.in1 = in1
-        self.in2 = in2
-        self.pwm = pwm
-        self.standbyPin = standbyPin
-        self.reverse = reverse
-        
-        GPIO.setup(self.in1, GPIO.OUT)
-        GPIO.setup(self.in2, GPIO.OUT)
-        GPIO.setup(self.pwm, GPIO.OUT)
-        GPIO.setup(self.standbyPin, GPIO.OUT)
-        self.p = GPIO.PWM(pwm, self.hertz)
-        self.p.start(0)
-        
-    # function responsible for the movement of the motor (with speed between -100 and 100)
-    def move(self, speed):
-        dutyCycle = speed
-        
-        # initiates reverse movement
-        if (speed < 0):
-            dutyCycle = dutyCycle * -1
-            
-        # outputs movement of the motor
-        if (speed > 0):
-            GPIO.output(self.in1, GPIO.HIGH)
-            GPIO.output(self.in2, GPIO.LOW)
-        else:
-            GPIO.output(self.in1, GPIO.LOW)
-            GPIO.output(self.in2, GPIO.HIGH)
-            
-        self.p.ChangeDutyCycle(dutyCycle)
-        
-    # stops the motor from moving
-    def stop(self):
-        self.p.ChangeDutyCycle(0)
-        GPIO.output(self.in1,GPIO.HIGH)
-        GPIO.output(self.in2,GPIO.HIGH)
-        
-    # standby ?? not exactly sure what the purpose of this is, will remain to be seen if we will keep
-    def standby(self):
-        self.p.ChangeDutyCycle(0)
-        GPIO.output(self.standbyPin, value)
-        
-    # destructor
-    def __del__(self):
-        GPIO.cleanup()
-'''
